325a37d19bfad61ff3e975b389923bb65ab9202f
DESKTOP-E4UEJB8\DELL <tazwar70@gmail.com>
2020-08-16 09:31:26 -0400

    initial

---
+++ assets/js/footer.js
@@ -0,0 +1,14 @@
+document.getElementById('footer').innerHTML =
+    `
+<ul class="icons">
+    <li><a href="https://twitter.com/TazwarH"><i class="fab fa-twitter"></i></a></li>
+    <li><a href="https://github.com/tazwar70?tab=repositories"><i class="fab fa-github"></i></a></li>
+    <li><a href="https://www.instagram.com/tajwarscribbles"><i class="fab fa-instagram"></i></a></li>
+    <li><a href="https://www.deviantart.com/tajwarscribbles"><i class="fab fa-deviantart"></i></a></li>
+    <li><a href="https://www.instagram.com/tazwar70"><i class="fab fa-instagram"></i></a></li>
+    <li><a href="https://www.buymeacoffee.com/ShotByTajwar"><i class="fad fa-coffee"></i></a></li>
+</ul>
+<ul class="copyright">
+    <li>&copy; Md Shadman Tajwar Haque</li><li>Sources: <a href="sources.html">Click Here</a></li>
+</ul>
+`
